54c9d091acb56a1c2b8d06227b313a8ef20cbd58
Karim Lentz <karim.lentz25@gmail.com>
2026-09-21 16:16:22 -0300

    Criado o método MC3( )

--- ClasseC.cpp
+++ ClasseC.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 class C {
 private:
-    std::string C1;
+    string C1;
     int C2;
 
 public:
-    std::string getC1() { return C1; }
-    void setC1(std::string c1) { C1 = c1; }
+    string getC1() { return C1; }
+    void setC1(string c1) { C1 = c1; }
 
     int getC2() { return C2; }
     void setC2(int c2) { C2 = c2; }
 
     void MC1() {
-        std::cout << "Metodo MC1" << std::endl;
+        cout << "Metodo MC1" << endl;
     }
 
     void MC2() {
-        std::cout << "Metodo MC2" << std::endl;
+        cout << "Metodo MC2" << endl;
+    }
+
+    void MC3() { 
+        cout << "Metodo MC3" << endl; 
     }
 };
